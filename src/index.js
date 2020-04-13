c65e820e233d36f7a605bb0d79d44626540df603
Stanislaw Antoniak <stanislaw.h.antoniak@gmail.com>
2020-04-13 10:55:12 +0200

    port

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+console.log('port: ',process.env.PORT);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
